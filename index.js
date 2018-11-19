7be4c09b7792446622dfd8dde1c51506880cf15a
Cara Morelli <caramorelli7@gmail.com>
2018-11-19 04:55:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file
+
+function findMatching(drivers, string) {
+   var match_arr = [];
+  for ( var i = 0; i < drivers.length; i++) {
+    var driver = (drivers[i]).toLowerCase()
+    if (driver.startsWith(string.toLowerCase())) {
+      match_arr.push(drivers[i]);
+    }
+  }
+  return match_arr;
+}
+
+function fuzzyMatch(drivers, string) {
+  var match_arr = [];
+  for ( var i = 0; i < drivers.length; i++) {
+    if (drivers[i].startsWith(string)) {
+      match_arr.push(drivers[i]);
+    }
+  }
+  return match_arr;
+}
+
+function matchName(drivers, string) {
+  var match_obj = {};
+  for (var keys in drivers) {
+    if ( drivers[name] == string ) {
+      match_obj
+    }
+  }
+  
+}
+
+// Write `matchName` - This function takes an array of `drivers` and a `string` as
+// arguments. In this function, each element of the `drivers` array is a
+// JavaScript object that has a property of `name`. The function should return
+// each element whose `name` property matches the provided `string` argument.
